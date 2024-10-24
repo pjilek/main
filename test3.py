a6feae7d0963124f978ffacbadefc83273c6158e
pjilek <pjilek@dwavesys.com>
2024-10-24 13:51:45 -0700

    test3-works

--- test3.py
+++ test3.py
@@ -14,34 +14,23 @@ def valid_squares(attack_bitboards):
                 moves_list.append((piece, square))
     return moves_list
 
-def build_cqm(attack_bitboards, opponent_bitboard):
+def build_cqm(attack_bitboards):    
     cqm = ConstrainedQuadraticModel()
     obj = BinaryQuadraticModel(vartype="BINARY")
-    x = {}
 
-    # add valid moves to obj
+    x = {}
     for piece, bitboard in attack_bitboards.items():
         for square in range(64):
-            sqr_weight = 0
-
             square_is_reachable = (bitboard >> (square)) & 1 == 1
-            if square_is_reachable: sqr_weight += -1
-            opponent_in_square = (opponent_bitboard >> (square)) & 1 == 1
-            if opponent_in_square: sqr_weight += -1
 
-            if sqr_weight != 0:  
-                obj += sqr_weight * Binary(piece + str(square))
+            if square_is_reachable:
                 x[(piece, square)] = Binary(piece + str(square))
+                obj += -x[(piece, square)]
 
-    # Objective: for infeasible solutions, focus on right number of shifts for employees
     cqm.set_objective(obj)
+    cqm.add_constraint(sum(x.values()) == 1)
 
-    print(valid_squares(attack_bitboards))
-
-    # ensure only one piece is moved
-    cqm.add_constraint(sum(x[piece, square] for piece, square in valid_squares(attack_bitboards)) == 1)
     return cqm
-
 def parse_output(qubo_output):
     output = []
     for el in qubo_output.record:
@@ -52,12 +41,10 @@ def parse_output(qubo_output):
     for i, label in enumerate(qubo_output.variables):
         bit = output[i]
         if bit: return label
-
 def get_move(attack_bitboards, opponent_bitboard, board_size = 64):
-    cqm = build_cqm(attack_bitboards, opponent_bitboard)
+    cqm = build_cqm(attack_bitboards)
     sampler = LeapHybridCQMSampler()
     sampleset = sampler.sample_cqm(cqm)
-    print(sampleset)
     feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)
     print(feasible_sampleset)
     return parse_output(feasible_sampleset)
