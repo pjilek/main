cc5f6242f6baf8dcfcb11abdcb8ec4a6f456216a
pjilek <pjilek@dwavesys.com>
2024-10-24 13:15:05 -0700

    fix-test3

--- test3.py
+++ test3.py
@@ -14,14 +14,24 @@ def valid_squares(attack_bitboards):
                 moves_list.append((piece, square))
     return moves_list
 
-def build_cqm(attack_bitboards):
+def build_cqm(attack_bitboards, opponent_bitboard):
     cqm = ConstrainedQuadraticModel()
     obj = BinaryQuadraticModel(vartype="BINARY")
+    x = {}
 
+    # add valid moves to obj
     for piece, bitboard in attack_bitboards.items():
         for square in range(64):
+            sqr_weight = 0
+
             square_is_reachable = (bitboard >> (square)) & 1 == 1
-            if square_is_reachable: obj += -Binary(piece + str(square))
+            if square_is_reachable: sqr_weight += -1
+            opponent_in_square = (opponent_bitboard >> (square)) & 1 == 1
+            if opponent_in_square: obj += -1
+
+            if sqr_weight != 0:  
+                obj += sqr_weight * Binary(piece + str(square))
+                x[(piece, square)] = Binary(piece + str(square))
 
     # Objective: for infeasible solutions, focus on right number of shifts for employees
     cqm.set_objective(obj)
@@ -47,6 +57,7 @@ def get_move(attack_bitboards, opponent_bitboard, board_size = 64):
     cqm = build_cqm(attack_bitboards)
     sampler = LeapHybridCQMSampler()
     sampleset = sampler.sample_cqm(cqm)
+    print(sampleset)
     feasible_sampleset = sampleset.filter(lambda row: row.is_feasible)
     print(feasible_sampleset)
     return parse_output(feasible_sampleset)
